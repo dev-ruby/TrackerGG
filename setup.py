341a4924be3eb44d8152e24cea69f6572db47dcf
DevRuby <60344644+dev-ruby@users.noreply.github.com>
2023-05-03 16:34:30 +0900

    Update version
    - setup.py: Update version to 2.6.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fs:
 
 setuptools.setup(
     name="TrackerGG",
-    version="2.5.0",
+    version="2.6.0",
     author="DevRuby",
     author_email="hiveruby@gmail.com",
     description="TrackerGG API Wrapper Library",
